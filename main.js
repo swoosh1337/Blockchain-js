bd6c77dfa9e02b2553143c30e61abf942ea17f64
Irakli Grigolia <igrigolia@wpi.edu>
2023-12-29 12:35:37 +0000

    Add PoW

--- main.js
+++ main.js
@@ -5,12 +5,23 @@ class Block {
         this.index = index
         this.timestamp = timestamp
         this.data = data
+        this.difficulty = 5
         this.previousHash = previousHash
         this.hash = this.calculateHash()
+        this.nonce = 0
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString()
+    }
+
+    mineBlock(difficulty){
+        console.log("mining block...")
+        while(this.hash.substring(0,this.difficulty) !== Array(this.difficulty + 1).join("0")){
+            this.nonce++
+            this.hash = this.calculateHash()
+        }
+        console.log("Block mined: " + this.hash)
     }
 }
 
@@ -30,7 +41,7 @@ class BlockChain {
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash
-        newBlock.hash = newBlock.calculateHash()
+        newBlock.mineBlock(this.difficulty)
         this.chain.push(newBlock)
     }
 
@@ -53,7 +64,7 @@ let blockchain = new BlockChain()
 blockchain.addBlock(new Block(1,'12/29/2023',{amount:4}))
 blockchain.addBlock(new Block(2,'12/29/2023',{amount:10}))
 blockchain.addBlock(new Block(3,'12/29/2023',{amount:15}))
+blockchain.addBlock(new Block(4,'12/29/2023',{amount:20}))
+blockchain.addBlock(new Block(5,'12/29/2023',{amount:20}))
 
-console.log(blockchain.isChainValid())
-
-// console.log(JSON.stringify(blockchain, null, 4))
+console.log(blockchain)
